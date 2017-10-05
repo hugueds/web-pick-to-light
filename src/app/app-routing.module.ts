424bc937f0920abc6d712d8387b6bb47ad54a499
Peres de Freitas <hugo.freitas@scania.com>
2017-10-05 12:12:06 -0300

    linting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ConfigurationComponent } from "./configuration/configuration.component";
-import { AboutComponent } from "./about/about.component";
+import { ConfigurationComponent } from './configuration/configuration.component';
+import { AboutComponent } from './about/about.component';
 
-import { ContentComponent } from "./content/content.component";
-import { DeviceGuard } from "./guard/device.guard";
+import { ContentComponent } from './content/content.component';
+import { DeviceGuard } from './guard/device.guard';
 import { ShelfConfigComponent } from './shelf-config/shelf-config.component';
 import { TestsComponent } from './tests/tests.component';
 import { ShelfDetailComponent } from './shelf-config/shelf-detail.component';
@@ -15,16 +15,16 @@ import { OpkDetailComponent } from './opk/detail/opk-detail.component';
 
 const routes: Routes = [
     { path: '', component: ContentComponent, canActivate : [DeviceGuard]  },
-    { path : 'shelf-config', component: ShelfConfigComponent },         
-    { path : 'shelf-config/create', component: ShelfDetailComponent },    
+    { path : 'shelf-config', component: ShelfConfigComponent },
+    { path : 'shelf-config/create', component: ShelfDetailComponent },
     { path : 'shelf-config/edit/:buttonId', component: ShelfDetailComponent },
-    { path : 'opk', component: OpkComponent },            
+    { path : 'opk', component: OpkComponent },
     { path : 'opk/edit/:partNumber', component: OpkDetailComponent },
-    { path : 'opk/create', component: OpkDetailComponent },        
+    { path : 'opk/create', component: OpkDetailComponent },
     { path: 'configuration', component: ConfigurationComponent },
     { path: 'about', component: AboutComponent },
-    {path: 'tests', component: TestsComponent}  
-]
+    {path: 'tests', component: TestsComponent}
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
